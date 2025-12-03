de388bd179bd023cffc601091a72e06141192e9c
Roman <romanplay@mail.ru>
2025-12-03 22:16:41 +0300

    add shitty splash you will never see as a library

--- Code/FMOD/RuntimeManager.3D.cs
+++ Code/FMOD/RuntimeManager.3D.cs
@@ -4,13 +4,9 @@ public partial class FMODManager
 	public static void SetListenerLocation( int listenerIndex, GameObject gameObject, GameObject attenuationObject = null, Vector3 velocity = new Vector3() )
 	{
 		if ( attenuationObject.IsValid() )
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.Transform.World, velocity ), RuntimeUtils.SourceToFMODVector( attenuationObject.WorldPosition ) );
-		}
 		else
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.Transform.World, velocity ) );
-		}
 	}
 
 	public static void SetListenerLocation( GameObject gameObject, GameObject attenuationObject = null )
@@ -21,13 +17,9 @@ public partial class FMODManager
 	public static void SetListenerLocation( int listenerIndex, GameObject gameObject, GameObject attenuationObject = null )
 	{
 		if ( attenuationObject.IsValid() )
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.WorldPosition ), RuntimeUtils.SourceToFMODVector( attenuationObject.WorldPosition ) );
-		}
 		else
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.WorldPosition ) );
-		}
 	}
 
 	public static void SetListenerLocation( GameObject gameObject, Rigidbody rigidBody, GameObject attenuationObject = null )
@@ -38,12 +30,8 @@ public partial class FMODManager
 	public static void SetListenerLocation( int listenerIndex, GameObject gameObject, Rigidbody rigidBody, GameObject attenuationObject = null )
 	{
 		if ( attenuationObject.IsValid() )
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.WorldTransform, rigidBody.WorldPosition ), RuntimeUtils.SourceToFMODVector( attenuationObject.WorldTransform.Position ) );
-		}
 		else
-		{
 			Instance.studioSystem.setListenerAttributes( listenerIndex, RuntimeUtils.To3DAttributes( gameObject.WorldTransform, rigidBody.WorldPosition ) );
-		}
 	}
 }
